6fac5225f057b576515cb93bbe97e51bb9eaa3ce
grisha9154 <grisha9154@gmail.com>
2022-01-28 22:53:34 +0300

    Update send answer

--- src/api/tg-router.ts
+++ src/api/tg-router.ts
@@ -5,20 +5,17 @@ import { messageService } from "../services/message-handler";
 const url = "https://api.telegram.org/bot";
 const token = process.env.tg_bot_token;
 
-
 export const tgRouter: RequestHandler = async (req, res) => {
   const { chat, text } = req.body.message;
-  const sendAswer = (answer: String) => axios.post(`${url}${token}/sendMessage`, {
-    chat_id: chat.id,
-    answer,
-  })
   try {
     const answer = await messageService.handelMessage(text);
-    await sendAswer(answer);
+    await axios.post(`${url}${token}/sendMessage`, {
+      chat_id: chat.id,
+      answer,
+    });
   } catch (error) {
-    const message = `Error with send`
+    const message = `Error with send`;
     console.error(message);
-    await sendAswer(message);
   }
 
   res.status(200).send({});
