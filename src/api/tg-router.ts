681c62f867ccef778a925fe4ece1900f382f8c41
grisha9154 <grisha9154@gmail.com>
2022-01-28 17:54:25 +0300

    Add handler in tgbot

--- src/api/tg-router.ts
+++ src/api/tg-router.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 
 export const tgRouter: RequestHandler = (req, res) => {
-  res.send('Люба ты самая красивая. Я тебя очень люблю');
+  const body = req.body;
+  console.log(body);
+  res.send(body);
 };
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { tgRouter } from "./api";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
 
 app.get("/tgbot", tgRouter);
 
