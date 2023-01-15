482a7a5a67b2f2b72f1a25d66af13ea1747e15c2
grisha9154 <grisha9154@gmail.com>
2023-01-15 14:24:26 +0300

    Cleare

--- src/api/index.ts
+++
@@ -1,3 +0,0 @@
-export { tgRouter } from './tg-router'
-export { uploadDataRouter } from './upload-data';
-export { costGoodsRouter } from './cost-goods';
--- src/services/token-handler/add-order-command-executer.ts
+++
@@ -1,9 +0,0 @@
-import { roundCost } from "../../utils/round-cost";
-import { mapParams, Param } from "../../utils/map-order-params";
-import { Order } from "../../data-base/models/order";
-
-export const execOrderAddCommand = async (params: Param[]) => {
-    const orderEntity = await mapParams(params);
-    await Order.create(orderEntity);
-    return `Добавлен заказ.\nСтоимость заказа ${roundCost(orderEntity.cost)}р\nСебестоимость ${roundCost(orderEntity.costGoods)}р.\nПрибыль ${roundCost(orderEntity.freeMony)}р`;
-};
